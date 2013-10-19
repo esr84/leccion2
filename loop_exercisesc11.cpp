daaa87eb5912d86879b249bb6f0e610e5299658c
esr84 <esruni@gmail.com>
2013-10-19 16:14:31 +0200

    Cambios

--- loop_exercisesc11.cpp
+++ loop_exercisesc11.cpp
@@ -10,12 +10,13 @@ void countFive();
 void loopString();
 map<char*, int> convertMap(map<string, float> mapa1);
 void convertMap2(map<string, float> &mapa1);
-
+void createPyramid(int n);
 
 // http://jessicarbrown.com/images/ft-cpp-loop.gif
 int main()
 {
-    map<string, float> mapa1;
+    // declaramos el mapa y lo rellenamos con el estilo de c++ 11 //
+    map<string, float> mapa1 = {{"hola mundo",1.2},{"que tal mundo",2.3},{"redondea hacia arriba",7.7},{"adios mundo",3.4}};
     map<char*, int> mapa2;
     
     // non-c++11 syntax:
@@ -34,17 +35,14 @@ int main()
     // new map of of (char*, int)
 
     cout << "\nejercicio 3.1:" << "\n";
-    mapa1.insert(pair<string,float>("hola mundo",1.2));
-    mapa1.insert(pair<string,float>("que tal mundo",2.3));
-    mapa1.insert(pair<string,float>("redondea hacia arriba",7.7));
-    mapa1.insert(pair<string,float>("adios mundo",3.4));
 
     mapa2 = convertMap(mapa1);
     
     cout << "\n" << "Mapa copiado: " << "\n";
-    for(map<char *,int>::iterator it1 = mapa2.begin();it1 != mapa2.end();it1++)
+    // recorre el mapa devuelto por la funcion anterior //
+    for(auto it1 : mapa2)
     {
-        cout << it1->first << "," << it1->second << "\n";
+        cout << it1.first << "," << it1.second << "\n";
     }
 
     // 3. create a for loop that gets a map of <string, float> and updates the
@@ -53,18 +51,18 @@ int main()
      cout << "\nejercicio 3.2:" << "\n"; 
     convertMap2(mapa1);
   
-    
-    for(map<string,float>::iterator it2 = mapa1.begin();it2 != mapa1.end();it2++)
+    // recorremos el mapa una vez cambiado en la funcion // 
+    for(auto it2 : mapa1)
     {
-        cout << it2->first << "," << it2->second << "\n";
+        cout << it2.first << "," << it2.second << "\n";
     }
 
     //----------------------------------------------------------------------
         
     // 4. transform every previous exercise to ranged for loops. use as much
     // c++11 syntax as you can
-    // EL FICHERO CON LA SINTASIX DE C++11 es loop_exercisesC11.cpp
-    
+    // EL FICHERO CON LOS BUCLES SIN UTILIZAR LA SINTASIX DE C++ 11 es el loop_exercises.cpp   
+ 
     // 5. write a for loop that prints a pyramid of height N, like this:    
     //	
     //	               **
@@ -74,21 +72,42 @@ int main()
     //	           **********
     //	          ************
     
+    cout << "\n";
+    createPyramid(6);
+    cout << "\n";
+    createPyramid(5);
+    cout << "\n";
+    createPyramid(10);
+}
+
+void createPyramid(int n)
+{
+    int i;
+
+    for(int c=0;c<n;c++)
+    {
+        for(i=0;i<n-c;i++)
+        {
+            cout << " ";
+        }
+        for(i=0;i<(c+1)*2;i++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+   }
 }
 
 void convertMap2(map<string, float> &mapa1)
 {
-
-    // crea el iterador y recorre el mapa //
-    for(map<string, float>::iterator it = mapa1.begin();it!=mapa1.end();it++)
+    // recorre el mapa con un bucle estili c++ 11 //
+    for(auto it : mapa1)
     {
-        // redondea el float//
-        it->second = round(it->second);
-        // cambia el valor del string //
-        it->first.assign("yeah!");
+        // redondeamos el float //
+        it.second = round(it.second);
+        // cambiamos el texto por yeah! //
+        it.first.assign("yeah!");
     }
-
-   //convertMap(mapa1); 
 }
 
 map<char*, int> convertMap(map<string, float> mapa1)
@@ -98,20 +117,18 @@ map<char*, int> convertMap(map<string, float> mapa1)
     // creamos una variable auxiliar para guardar los char * //
     char *aux;
 
-    // crea el iterador para el mapa //
-    map<string, float>::iterator it = mapa1.begin();
-
     cout << "\n" << "Mapa original:" << "\n";
-    for(;it != mapa1.end();it++)
+    // bucle estilo c++11 //
+    for(auto it : mapa1)
     {
         // imprime por pantalla la posicion actual del mapa //
-        cout << it->first << "," << it->second << "\n";
+        cout << it.first << "," << it.second << "\n";
         // reservamos memoria para la cadena de carateres //
-        aux = new char[it->first.length()];
+        aux = new char[it.first.length()];
         // copiamos el string al char * //
-	strcpy(aux,it->first.c_str());
+	strcpy(aux,it.first.c_str());
         // copiamos la posicion nueva del map al nuevo map //
-        mapa2.insert(pair<char*,int>(aux,(int)it->second));
+        mapa2.insert(pair<char*,int>(aux,(int)it.second));
     }
 
     // devolvemos el valor //
@@ -124,10 +141,10 @@ void loopString()
     // crea el vecor inicializandolo a 5 strings //
     vector<string> vec = {"hola", ",Que"," tal" ," estas", ". Adios"};
 
-    // declara iterador en el for e inicia la iteracion //
-    for(vector<string>::iterator it = vec.begin(); it!=vec.end();it++)
+    // utilizamos un bucle estilo c++ 11 //
+    for(auto it : vec)
     {
-        cout << *it ;
+        cout << it ;
     }
 
     cout << "\n";
--- makefile
+++ makefile
@@ -1,10 +1,14 @@
 # makefile hecho a mano
 
-all: loop
+all: loop loopC11
 
 loop: loop_exercises.cpp 
 	g++ -lm -std=c++11 -fpermissive $< -o $@
 
+loopC11: loop_exercisesc11.cpp
+	g++ -lm -std=c++11 -fpermissive $< -o $@
+
 clean:
 	rm -f loop
+	rm -f loopC11
 	rm -f *.o
